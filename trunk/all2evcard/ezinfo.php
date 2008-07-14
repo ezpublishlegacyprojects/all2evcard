ae410028a23c2456c6e43b034e51b3004aee89ef
ngoers <info@example.com>
2008-07-14 14:00:10 +0000

    ezinfo.php added
    git-svn-id: http://svn.projects.ez.no/all2evcard@3 4ff8cc84-fb17-4fd9-b1fa-970e5f38793a

---
+++ trunk/all2evcard/ezinfo.php
@@ -0,0 +1,17 @@
+<?php
+class all2evcardInfo
+{
+    /*!
+     Constructor
+    */
+    function info()
+    {
+        return array( 'Name' => "vCard management",
+                      'Version' => "1.0",
+                      'Copyright' => "Copyright (C) 2008 <a href='http://all2e.com'>all2e GmbH</a>",
+                      'License' => "GNU General Public License v2.0"
+                     );
+    }
+}    
+
+?>
